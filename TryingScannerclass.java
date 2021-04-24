cda140b3fcfa8c508284c60cae8e0337dc462730
prathyushacodes <prathyushaeerpini@gmail.com>
2021-04-23 17:40:11 -0700

    tryingscannerclassprgrm1

---
+++ TryingScannerclass.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+ class TryingScannerclass{
+    public static void main(String args[]){
+        Scanner abc =new Scanner(System.in);
+        System.out.println("Enter username: ");
+        String username=abc.nextLine();
+        System.out.println("Username is : "+username);
+
+    }
+
+    
+
+}
