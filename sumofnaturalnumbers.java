b8a5ecaff652152ab16394f1a6d849609f0f3718
prathyushacodes <prathyushaeerpini@gmail.com>
2021-04-25 15:08:07 -0700

    natural numbers 100 sum

---
+++ sumofnaturalnumbers.java
@@ -0,0 +1,13 @@
+public class sumofnaturalnumbers {
+    public static void main(String args[]){
+        int i=1, num=100;
+        int sum=0;
+        for(i=1;i<=num;i++){
+
+            sum=sum+i;
+
+        }
+        System.out.println("The sum of n natural numbers is " + sum);
+    }
+    
+}
