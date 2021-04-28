ef2145087df4f5ce5c8daaa7d609858e14415128
PrathyushaCodes <prathyushaeerpini@gmail.com>
2021-04-27 17:23:28 -0700

    gcd of 2 numbers using while

--- GCDoftwonum.java
+++ GCDoftwonum.java
@@ -1,6 +1,19 @@
 public class GCDoftwonum {
-    public static void main(String args[]){
-       
+        public static void main(String[] args) {
+            
+            int num1 = 44, num2 = 36;
+            
+                    while (num1 != num2) {
+                        if(num1 > num2)
+                            num1 = num1 - num2;
+                        else
+                            num2 = num2 - num1;
+                    }
+            
+                    System.out.println("GCD of given numbers is: " + num2);
+                
+            
+            
+        }
     }
-    
-}
+          
