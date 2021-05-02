8f534014f9683ccf527e24ee3081ef82c24a3f7b
PrathyushaCodes <prathyushaeerpini@gmail.com>
2021-05-02 09:12:18 -0700

    gcd

--- GCDoftwonum.java
+++ GCDoftwonum.java
@@ -1,7 +1,7 @@
 public class GCDoftwonum {
         public static void main(String[] args) {
             
-            int num1 = 44, num2 = 36;
+            int num1 = 4, num2 = 6;
             
                     while (num1 != num2) {
                         if(num1 > num2)
