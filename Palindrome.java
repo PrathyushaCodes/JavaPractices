0968bab5fdbef34daaec369e2d6b6f70fa0d0ae2
PrathyushaCodes <prathyushaeerpini@gmail.com>
2021-05-15 16:49:17 -0700

    committing

--- Palindrome.java
+++ Palindrome.java
@@ -1,7 +1,7 @@
-public class Palindrome {
+public class e {
     public static void main(String args[]){  
          int r,sum=0,temp;    
-         int n=123;//It is the number variable to be checked for palindrome  
+         int n=123;//It is the number variable to be checked for e  
          
          temp=n;    
          while(n>0){    
@@ -10,9 +10,9 @@ public class Palindrome {
           n=n/10;    
          }    
          if(temp==sum)    
-          System.out.println(" given number is a palindrome number ");    
+          System.out.println(" given number is a e number ");    
          else    
-          System.out.println("given number is not palindrome number");    
+          System.out.println("given number is not e number");    
        }  
         
 }
